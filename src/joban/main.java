d97e92dd873ac02f933c37193f65a4c963b75bc9
747624 <747624@123-PC31.student.Tor.Loc>
2019-05-28 14:23:57 -0400

    intial co

---
+++ src/joban/main.java
@@ -0,0 +1,18 @@
+package joban;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Scanner keyboard = new Scanner(System.in);  // Create a Scanner object
+		    System.out.println("Enter number of weeks");	
+		
+		int numberofweeks = keyboard.nextInt() ;
+		
+		System.out.println(" number of weeks:" +numberofweeks);	
+	}
+
+}
